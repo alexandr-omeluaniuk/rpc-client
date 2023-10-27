2ce89e51f431c10e8ad884eafb13b6c09d6d2af3
Alex Amelyaniuk <starshistrelok@gmail.com>
2023-10-27 18:40:31 +0300

    Refactoring

--- module/libs/rpc-transport-http/src/main/kotlin/ss/rpc/transport/http/RpcHttpClient.kt
+++ module/libs/rpc-transport-http/src/main/kotlin/ss/rpc/transport/http/RpcHttpClient.kt
@@ -1,6 +1,7 @@
 package ss.rpc.transport.http
 
 import org.slf4j.LoggerFactory
+import ss.rpc.core.RpcCallSignature
 import ss.rpc.core.api.RpcClientProxy
 import ss.rpc.core.state.RoutingTable
 import java.lang.reflect.Method
@@ -10,10 +11,15 @@ class RpcHttpClient : RpcClientProxy {
     private val logger = LoggerFactory.getLogger(RpcHttpClient::class.java)
 
     override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any {
-        logger.debug("Call RPC:$method")
+        logger.debug("Call RPC [$method]")
         logger.debug("Arguments: ")
         args?.forEach { logger.debug(it.toString()) }
-        val route = RoutingTable.getInstance().getRoute(method.toString())
+        val rpcSignature = RpcCallSignature(
+            rpcService = method!!.declaringClass,
+            method = method!!
+        ).toString()
+        logger.debug("RPC signature [$rpcSignature]")
+        val route = RoutingTable.getInstance().getRoute(rpcSignature)
         logger.debug(route.toString())
         return 0
     }
--- module/microservice/discovery-microservice/src/main/kotlin/ss/rpc/discovery/service/DiscoveryService.kt
+++ module/microservice/discovery-microservice/src/main/kotlin/ss/rpc/discovery/service/DiscoveryService.kt
@@ -1,5 +1,6 @@
 package ss.rpc.discovery.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import ss.rpc.core.state.RoutingTable
 import ss.rpc.core.state.RpcRoute
@@ -8,9 +9,13 @@ import ss.rpc.discovery.core.RpcRegistrationInfo
 @Service
 class DiscoveryService {
 
+    private val logger = LoggerFactory.getLogger(DiscoveryService::class.java)
+
     fun registerRpcCall(rpcRegistrationInfo: RpcRegistrationInfo, host: String): List<RpcRoute> {
         val routes = rpcRegistrationInfo.signatures.map {
-            RpcRoute(it, host, rpcRegistrationInfo.port)
+            RpcRoute(it, host, rpcRegistrationInfo.port).also { route ->
+                logger.debug("Register new route [$route]")
+            }
         }
         val routingTable = RoutingTable.getInstance()
         routingTable.append(routes)

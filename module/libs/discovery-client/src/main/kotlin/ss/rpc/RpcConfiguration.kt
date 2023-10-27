80bc34d0773126f2a553f119c2cd38e57c32ce05
Alex Amelyaniuk <starshistrelok@gmail.com>
2023-10-27 18:50:02 +0300

    Refactoring

--- module/libs/discovery-client/src/main/kotlin/ss/rpc/RpcConfiguration.kt
+++ module/libs/discovery-client/src/main/kotlin/ss/rpc/RpcConfiguration.kt
@@ -1,57 +1,25 @@
 package ss.rpc
 
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
 import org.springframework.beans.factory.support.BeanDefinitionRegistry
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver
-import org.springframework.core.type.classreading.CachingMetadataReaderFactory
-import org.springframework.util.ClassUtils
-import ss.rpc.core.RpcService
 import ss.rpc.core.api.RpcClientProxy
+import ss.rpc.util.RpcServicesScanner
 import java.lang.reflect.Proxy
 import java.util.*
 
 @Configuration
 open class RpcConfiguration() : BeanDefinitionRegistryPostProcessor {
 
-    private val logger = LoggerFactory.getLogger(RpcConfiguration::class.java)
-
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
 
     }
 
     override fun postProcessBeanDefinitionRegistry(registry: BeanDefinitionRegistry) {
-        val rpcClients = scanForRpcClients()
+        val rpcClients = RpcServicesScanner().findClients()
         rpcClients.forEach { registerRpcClientAsBean(it, registry) }
-    }
-
-    private fun scanForRpcClients(): Set<Class<Any>> {
-        val classPackage = javaClass.packageName
-        val packageSearchPath = "classpath*:${classPackage.replace(".", "/")}/**/*.class"
-        val resources = PathMatchingResourcePatternResolver().getResources(packageSearchPath).filter {
-            !(it.filename?.contains(ClassUtils.CGLIB_CLASS_SEPARATOR) ?: true)
-        }
-        logger.debug("[${resources.size}] Spring resources detected by search path [$packageSearchPath]")
-        val metadataReaderFactory = CachingMetadataReaderFactory()
-        val classes = resources.map {
-            Class.forName(metadataReaderFactory.getMetadataReader(it).classMetadata.className) as Class<Any>
-        }
-        val rpcServices = classes.filter { cl ->
-            cl.isInterface && cl.getAnnotation(RpcService::class.java) != null
-        }
-        logger.debug("Total RpcService interfaces detected [${rpcServices.size}]")
-        val implementedInterfaces = classes.mapNotNull {  cl ->
-            when (cl.isInterface) {
-                true -> null
-                false -> cl.interfaces.asList()
-            }
-        }.flatten()
-        return rpcServices.filter { it -> !implementedInterfaces.contains(it) }.toSet().also {
-            it.forEach { logger.debug("RpcService client detected [$it]") }
-        }
     }
 
     private fun registerRpcClientAsBean(rpcClientInterface: Class<Any>, registry: BeanDefinitionRegistry) {
---
+++ module/libs/discovery-client/src/main/kotlin/ss/rpc/util/RpcServicesScanner.kt
@@ -0,0 +1,44 @@
+package ss.rpc.util
+
+import org.slf4j.LoggerFactory
+import org.springframework.core.io.Resource
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory
+import org.springframework.util.ClassUtils
+import ss.rpc.core.RpcService
+
+class RpcServicesScanner {
+
+    private val logger = LoggerFactory.getLogger(RpcServicesScanner::class.java)
+
+    fun findClients(): Set<Class<Any>> {
+        val resources = searchResources()
+        val metadataReaderFactory = CachingMetadataReaderFactory()
+        val classes = resources.map {
+            Class.forName(metadataReaderFactory.getMetadataReader(it).classMetadata.className) as Class<Any>
+        }
+        val rpcServices = classes.filter { cl ->
+            cl.isInterface && cl.getAnnotation(RpcService::class.java) != null
+        }
+        logger.debug("Total RpcService interfaces detected [${rpcServices.size}]")
+        val implementedInterfaces = classes.mapNotNull {  cl ->
+            when (cl.isInterface) {
+                true -> null
+                false -> cl.interfaces.asList()
+            }
+        }.flatten()
+        return rpcServices.filter { !implementedInterfaces.contains(it) }.toSet().also {
+            it.forEach { logger.debug("RpcService client detected [$it]") }
+        }
+    }
+
+    private fun searchResources(): List<Resource> {
+        val classPackage = javaClass.packageName
+        val packageSearchPath = "classpath*:${classPackage.replace(".", "/")}/**/*.class"
+        return PathMatchingResourcePatternResolver().getResources(packageSearchPath).filter {
+            !(it.filename?.contains(ClassUtils.CGLIB_CLASS_SEPARATOR) ?: true)
+        }.also {
+            logger.debug("[${it.size}] Spring resources detected by search path [$packageSearchPath]")
+        }
+    }
+}
